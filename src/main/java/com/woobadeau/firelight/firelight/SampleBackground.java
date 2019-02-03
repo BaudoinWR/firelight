8db3782c26a61d74f186b8c91a5441ea5cfb4837
Baudoin Woods Ribadeau <baudoin.wr@gmail.com>
2019-02-03 14:19:05 +0100

    Scale background

--- src/main/java/com/woobadeau/firelight/firelight/SampleBackground.java
+++ src/main/java/com/woobadeau/firelight/firelight/SampleBackground.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 
 public class SampleBackground {
     public SampleBackground() throws IOException {
-        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0000.png"), 6);
-        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0001.png"), 4);
-        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0002.png"), 2);
-        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0003.png"), 1);
-        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0004_Sky.png"), 0);
+        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0000.png"), 6).scale(1024,740);
+        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0001.png"), 4).scale(1024,740);
+        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0002.png"), 2).scale(1024,740);
+        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0003.png"), 1).scale(1024,740);
+        new ScrollingBackground(TinyEngine.uiInterfaceProvider.getImage("/Hills__0004_Sky.png"), 0).scale(1024,740);
     }
 
 }
